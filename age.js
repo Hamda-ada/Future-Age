51f37505f2a843cd81836f7323c27bbe79b30d11
Hamda-ada <167078844+Hamda-ada@users.noreply.github.com>
2024-04-15 14:19:05 +0000

    Practise challenge

---
+++ age.js
@@ -0,0 +1,9 @@
+let age = 50;
+let retirementage = 67;
+let futureage = retirementage - age;
+console.log(`In ${futureage} years, you can retire.`);
+
+const readline= require("readline-sync");
+let userAge= readline.question("Please enter your age: ")
+console.log(userAge + 10)
+//what year do you want to know how old you are//
